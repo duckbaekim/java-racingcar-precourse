dd4c92a5f531ce42ec0c8541cb86f8bb4496429a
inqu <>
2021-05-04 03:54:32 +0900

    feat($console): 자동차 클래스 구현 및 이동 테스트 작성
    자동차의 이름과 위치를 받는 Car클래스 생성 및
    자동차의 포지션을 변경해줄 move메소드 생성 및 테스트 코드 작성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,4 +3,20 @@ package racingcar;
 public class Car {
 
 
+    public static final int DEFAULT_POSITION = 0;
+    private String name;
+    private int position;
+
+    public Car(String name){
+        this.name =name;
+        this.position = DEFAULT_POSITION;
+    }
+
+    public void move() {
+        this.position++;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,9 +1,21 @@
 package racingcar;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CarTest {
 
+    @Test
+    @DisplayName("자동차 이동 테스트")
+    void go_car(){
+        Car car = new Car("inqu");
+        car.move();
+        assertEquals(car.getPosition(), 1);
+        car.move();
+        assertEquals(car.getPosition(), 2);
+    }
 }
 
 
