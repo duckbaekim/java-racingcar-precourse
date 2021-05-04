450a428c7e73fd9d8ebfb761bf191799d1606d8d
inqu <>
2021-05-04 21:10:42 +0900

    feat($console): 이동 횟수 랜덤으로 받는 함수 생성
    자동차의 이동횟수를 입력 받아 랜덤 숫자 배열로 리턴하는 함수 생성
    테스트 코드 작성 및 테스트 완료

--- src/main/java/racingcar/ValidationUtils.java
+++ src/main/java/racingcar/ValidationUtils.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import java.util.Random;
+
 public class ValidationUtils {
 
     public static final int MAX_NAME_LENGTH = 5;
@@ -9,5 +11,14 @@ public class ValidationUtils {
     public static boolean validCarName(String name) {
         int len = name.length();
         return len > MIN_NAME_LENGTH && len <= MAX_NAME_LENGTH;
+    }
+
+    public static int[] getRandomNumber(int randomSize) {
+        Random random = new Random();
+        int[] result = new int[randomSize];
+        for (int i = 0; i < randomSize; i++) {
+            result[i] = random.nextInt(10) - 1;
+        }
+        return result;
     }
 }
--- src/test/java/racingcar/ValidationUtilsTest.java
+++ src/test/java/racingcar/ValidationUtilsTest.java
@@ -4,8 +4,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ValidationUtilsTest {
+    public static final int MIN_RANDOM_NUMBER = 1;
+    public static final int MAX_RANDOM_NUMBER = 9;
+
     @Test
     @DisplayName("차 이름 검증")
     void valid_car_name() {
@@ -13,5 +17,16 @@ public class ValidationUtilsTest {
         assertThat(ValidationUtils.validCarName("aasdf")).isTrue();
         assertThat(ValidationUtils.validCarName("aasdfd")).isFalse();
     }
+
+    @Test
+    @DisplayName("랜덤 숫자 테스트")
+    void valid_random_number() {
+        int[] randomNumbers = ValidationUtils.getRandomNumber(5);
+        assertEquals(randomNumbers.length, 5);
+        for (int randomNumber : randomNumbers) {
+            assertThat(randomNumber >= MIN_RANDOM_NUMBER && randomNumber <= MAX_RANDOM_NUMBER).isTrue();
+        }
+    }
+
 
 }
